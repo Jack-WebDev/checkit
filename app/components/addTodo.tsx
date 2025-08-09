63bcddd65c1c1a88fdffd217784e404fc1ae87aa
Jack <jackwebdev31@gmail.com>
2025-08-09 15:24:20 +0200

    add a timeout  for reload

--- app/components/addTodo.tsx
+++ app/components/addTodo.tsx
@@ -76,7 +76,10 @@ export default function AddTodo() {
       } else {
         toast.success("Todo added successfully!");
         form.reset();
-        location.reload();
+
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
       }
     } catch (error) {
       console.log(error);
